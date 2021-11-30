563a550b699b2574d4f9be151f2c1a8f363173dd
PB <brouzda@datasys.cz>
2021-11-30 12:17:12 +0100

    zacisteni kodu, osetreni potencialnich chyb

--- PredpovedAlojz.cpp
+++ PredpovedAlojz.cpp
@@ -5,6 +5,9 @@
 
 #include "src/fonts/fonts.h"
 
+// maximalni stari dat v sekundach
+#define DATA_MAX_AGE 14400
+
 PredpovedAlojz::PredpovedAlojz( raLogger * logger, raConfig * config,  DataAplikace * dataAplikace )
 {
     this->logger = logger;
@@ -32,7 +35,7 @@ void PredpovedAlojz::loadData()
 
     if( httpResponseCode==200 ) {
         // Parse response
-        DynamicJsonDocument * doc = new DynamicJsonDocument(10000);
+        DynamicJsonDocument * doc = new DynamicJsonDocument(4000);
         DeserializationError de = deserializeJson( *doc, http.getStream());
 
         if( !de ) {
@@ -56,13 +59,20 @@ void PredpovedAlojz::loadData()
 
 boolean PredpovedAlojz::hasData()
 {
-    return this->jsonData!=NULL;
+    if( this->jsonData!=NULL 
+        && ( time(NULL) - this->casStazeni ) < DATA_MAX_AGE ) {
+          return true;
+    } else {
+      return false;
+    }
 }
 
 
+#define BUFFER_SIZE 100
+
 void PredpovedAlojz::drawData( ExtDisplay * extdisplay, boolean firstRun )
 {
-    char buffer[512];
+    char buffer[BUFFER_SIZE];
     const char * nadpis;
     const char * text;
     const char * created;
@@ -72,64 +82,77 @@ void PredpovedAlojz::drawData( ExtDisplay * extdisplay, boolean firstRun )
 
     extdisplay->setFont( fnt_YanoneSB13() );
 
+    if( firstRun ) {
+        this->logger->log( "Alojz:" );
+    }
+
     // vetsinu dne ukazujeme jen jednodenni predpoved; ale 16-20 ukazujeme na dnes i zitra
     time_t now = time(NULL);
     struct tm * cas = localtime( &now );
-    if( cas->tm_hour >= 16 && cas->tm_hour <= 20 ) {
-        if( firstRun ) {
-            this->logger->log( "Alojz:" );
-        }
+    if( cas->tm_hour >= 16 && cas->tm_hour < 20 ) {
+
+        int x_offset = 0;
 
         nadpis = (*this->jsonData)["day1"]["today_tomorrow"];
         text = (*this->jsonData)["day1"]["string"];
-        if( firstRun ) {
-            this->logger->log( "> %s: %s", nadpis, text );
+        created = (*this->jsonData)["day1"]["created"];
+        if( nadpis!=NULL && text!=NULL ) {
+          if( firstRun ) {
+              this->logger->log( "created: %s", created );
+              this->logger->log( "> '%s': '%s'", nadpis, text );
+          }
+
+          extdisplay->display->setTextColor( BARVA_NADPISU );
+          snprintf( buffer, BUFFER_SIZE, "%s:", nadpis );
+          buffer[BUFFER_SIZE-1]=0;
+          buffer[0] = toupper(buffer[0]);
+          x_offset = extdisplay->printUTF8( buffer );
+
+          extdisplay->display->setTextColor( BARVA_TEXTU );
+          x_offset = extdisplay->printUTF8( text, x_offset );
         }
-
-        extdisplay->display->setTextColor( BARVA_NADPISU );
-        snprintf( buffer, 512, "%s:", nadpis );
-        buffer[511]=0;
-        buffer[0] = toupper(buffer[0]);
-        int x_offset = extdisplay->printUTF8( buffer );
-
-        extdisplay->display->setTextColor( BARVA_TEXTU );
-        x_offset = extdisplay->printUTF8( text, x_offset );
 
         nadpis = (*this->jsonData)["day2"]["today_tomorrow"];
         text = (*this->jsonData)["day2"]["string"];
-        if( firstRun ) {
-            this->logger->log( "> %s: %s", nadpis, text );
+        created = (*this->jsonData)["day2"]["created"];
+        if( nadpis!=NULL && text!=NULL ) {
+          if( firstRun ) {
+              this->logger->log( "created: %s", created );
+              this->logger->log( "> '%s': '%s'", nadpis, text );
+          }
+
+          extdisplay->display->setTextColor( BARVA_NADPISU );
+          snprintf( buffer, BUFFER_SIZE, "%s: ", nadpis );
+          buffer[BUFFER_SIZE-1]=0;
+          buffer[0] = toupper(buffer[0]);
+          x_offset = extdisplay->printUTF8( buffer, x_offset );
+
+          extdisplay->display->setTextColor( BARVA_TEXTU );
+          x_offset = extdisplay->printUTF8( text, x_offset );
         }
-
-        extdisplay->display->setTextColor( BARVA_NADPISU );
-        snprintf( buffer, 512, "%s: ", nadpis );
-        buffer[511]=0;
-        buffer[0] = toupper(buffer[0]);
-        x_offset = extdisplay->printUTF8( buffer, x_offset );
-
-        extdisplay->display->setTextColor( BARVA_TEXTU );
-        x_offset = extdisplay->printUTF8( text, x_offset );
 
     } else {
 
         nadpis = (*this->jsonData)[prefer]["today_tomorrow"];
         text = (*this->jsonData)[prefer]["string"];
+        created = (*this->jsonData)[prefer]["created"];
 
-        if( firstRun ) {
-            this->logger->log( "Alojz:" );
-            this->logger->log( "prefer: %s", prefer );
-            this->logger->log( "created: %s", (*this->jsonData)[prefer]["created"] );
-            this->logger->log( "> %s: %s", nadpis, text );
+        if( nadpis!=NULL && text!=NULL ) {
+          if( firstRun ) {
+              this->logger->log( "prefer: %s", prefer );
+              this->logger->log( "created: %s", created );
+              this->logger->log( "> '%s': '%s'", nadpis, text );
+          }
+
+          extdisplay->display->setTextColor( BARVA_NADPISU );
+          snprintf( buffer, BUFFER_SIZE, "%s:", nadpis );
+          buffer[BUFFER_SIZE-1]=0;
+          buffer[0] = toupper(buffer[0]);
+          int x_offset = extdisplay->printUTF8( buffer );
+
+          extdisplay->display->setTextColor( BARVA_TEXTU );
+          extdisplay->printUTF8( text, x_offset );
         }
-
-        extdisplay->display->setTextColor( BARVA_NADPISU );
-        snprintf( buffer, 512, " %s:", nadpis );
-        buffer[511]=0;
-        buffer[0] = toupper(buffer[0]);
-        int x_offset = extdisplay->printUTF8( buffer );
-
-        extdisplay->display->setTextColor( BARVA_TEXTU );
-        extdisplay->printUTF8( text, x_offset );
     }
 
 }
--- README.md
+++ README.md
@@ -1,8 +1,1 @@
 
-
-Editor fontu (zejmena pro ikony a doplneni znaku "stupen" na pozizi 0x7e, tedy '~')
-https://tchapi.github.io/Adafruit-GFX-Font-Customiser/
-
-Generatory fontu:
-http://oleddisplay.squix.ch/#/home
-https://rop.nl/truetype2gfx/
